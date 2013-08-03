9164d18752e101609cf6d46c76d7b3128408ca89
Superloup10 <Superloup10@gmail.com>
2013-08-04 00:37:29 +0200

    Correction de bugs et mis en place des métadatas

--- ere_geologique/common/block/Slab.java
+++ ere_geologique/common/block/Slab.java
@@ -1,61 +1,71 @@
 package ere_geologique.common.block;
 
+import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.BlockHalfSlab;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import ere_geologique.common.EGCreativeTab;
+import ere_geologique.common.EreGeologique;
 import ere_geologique.common.config.EGProperties;
 
 public class Slab extends BlockHalfSlab
 {
+	public static final String[] woodType = new String[] {"fougere", "cycas", "araucarias", "metasequoias", "ginkgos"};
+	
 	public Slab(int par1, boolean par2)
 	 
 	{
 	super(par1, par2, Material.wood);
 	this.setCreativeTab(EGCreativeTab.EGCreativeTab);
-	setLightOpacity(0);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int par1, int par2)
+	{
+		return EGBlockList.Plank.getIcon(par1, par2 & 7);
 	}
 	
 	public int idDropped(int par1, Random par2Random, int par3)
 	{
 	return EGProperties.SlabID;
 	}
-	 
-	public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLiving par5EntityLiving)
-	{
-	if(par1World.getBlockId(par2, par3 - 1, par4) == EGProperties.SlabID)
-	{
-	par1World.setBlock(par2, par3, par4, 0);
-	par1World.setBlock(par2, par3 - 1, par4, EGProperties.DoubleSlabID);
-	}
-	}
-	 
+	
 	protected ItemStack createStackedBlock(int par1)
 	{
 	return new ItemStack(EGProperties.SlabID, 2, par1 & 7);
 	}
-	 
-	public String getFullSlabName(int var1)
+	
+	public String getFullSlabName(int par1)
 	{
-	return null;
+		if (par1 < 0 || par1 >= woodType.length)
+		{
+			par1 = 0;
+		}
+		
+		return super.getUnlocalizedName() + "." + woodType[par1];
 	}
-	 
-	@Override
-    public void registerIcons(IconRegister par1IconRegister)
-    {
-		blockIcon = par1IconRegister.registerIcon("EreGeologique:Blocks");
-    }
+	
+	public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
+	{
+		if (par1 != EGProperties.SlabID)
+		{
+			
+			for (int j = 0; j < 5; ++j)
+			{
+			par3List.add(new ItemStack(par1, 1, j));
+			}
+		}
+	}
 	 
 	@SideOnly(Side.CLIENT)
-	public int getBlockTextureFromSide(int i)
-	{
-	return 0;
-	}
+    public void registerIcons(IconRegister par1IconRegister) {}	 
 }
